130083e83fda0bc63f7568d67edab076c8c58632
Kiran <premkumarkirankumar@gmail.com>
2022-03-15 17:19:55 -0400

    updated code

--- src/test/java/petClinic/TestHomePagePROD.java
+++ src/test/java/petClinic/TestHomePagePROD.java
@@ -30,7 +30,7 @@ public class TestHomePagePROD {
 		System.out.println("Current URL:"+url );
 		System.out.println("Retrieved Text:"+findOwners);
 		boolean validation=false;
-		if(findOwners.contentEquals("FIND 11OWNERS")) {
+		if(findOwners.contentEquals("FIND 12OWNERS")) {
 			validation=true;
 		}
 		Assert.assertTrue(validation );
